62083f4cc3ca98bd6586c2a077d9b2f8b1d418c9
palmbeach05 <5945226+palmbeach05@users.noreply.github.com>
2026-06-09 09:13:42 -0400

    j5-common: Move system_prop values to j5-common
    There were values that were in the variant system_prop.mk that were
    common amongst all of the j5 2015 devices. It would be more appropriate
    to have them here.

--- system_prop.mk
+++ system_prop.mk
@@ -7,3 +7,11 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.vc_call_vol_steps=15 \
 	ro.config.media_vol_steps=30
+
+# CABL
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.qualcomm.cabl=2
+
+# RAM
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.low_ram=false
