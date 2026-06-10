cfee28132eabcd93004974798591ab1521e2087c
palmbeach05 <5945226+palmbeach05@users.noreply.github.com>
2026-06-09 23:03:51 -0400

    j5-common: Cleanup
    This cleans up most if not all of the unnecessary resources within the
    j5-common directory. Also, RIL values are not needed for TWRP, and
    omni.dependencies are present in msm8916-common, j5-common, and in each
    variant's directory. Let's get rid of the redundant duplicity to
    eliminate any dependency conflicts, synchronization headaches, and
    redundant manifest parsing during workspace syncs.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,22 +18,6 @@ include device/samsung/msm8916-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/samsung/j5-common
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
-
-# Include
-TARGET_SPECIFIC_HEADER_PATH += $(DEVICE_PATH)/include
-
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_j5
 TARGET_RECOVERY_DEVICE_MODULES := libinit_j5 tzdata
-
-# Lights
-TARGET_PROVIDES_LIBLIGHT := false
-
-# RIL
-BOARD_MODEM_TYPE := xmm7260
-BOARD_PROVIDES_LIBRIL := true
-
-# Wifi
-BOARD_HAVE_SAMSUNG_WIFI := true
--- device-common.mk
+++ device-common.mk
@@ -13,7 +13,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from common
 $(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
@@ -28,56 +27,12 @@ PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_DENSITY := 320
 
-# Common overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
-
-# Camera
-PRODUCT_PACKAGES += \
-    Camera2
-
-# Doze
-PRODUCT_PACKAGES += \
-    SamsungDoze
-
-# FlipFlap
-PRODUCT_PACKAGES += \
-    FlipFlap
-
-# Media configurations
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    $(DEVICE_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
-
 # Proprieties
 -include $(DEVICE_PATH)/system_prop.mk
 
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.target.rc
-
-# RIL
-PRODUCT_PACKAGES += \
-    libril \
-    librilutils \
-    rild
-
-# append the updater uri to the product properties if set
-ifneq ($(CM_UPDATER_OTA_URI),)
-    PRODUCT_PROPERTY_OVERRIDES += $(CM_UPDATER_OTA_URI)
-endif
 
 # Inhert dalvik heap values from aosp
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
